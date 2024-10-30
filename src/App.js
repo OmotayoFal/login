10601142826f8b16da6f9ec355367c753b34f9ad
OmotayoFal <faluyiomotayo05@gmail.com>
2024-10-30 17:31:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Login from "./Components/Login/Login";
 import Analytics from "./Components/Analytics/Analytics"; 
 import Projects from "./Components/Projects/Projects"; 
-import ToDo from './Components/ToDo/ToDo';
+import ToDoList from './Components/ToDo/ToDo';
 import Home from "./Components/Home/Home"; // Home contains the sidebar and sub-routes
 import Register from './Components/Login/Register';
 import ProfilePage from './Components/user/ProfilePage';  // Import the ProfilePage component
@@ -20,6 +20,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />  {/* Route for the dashboard (contains sidebar) */}
           <Route path="/register" element={<Register />} />
           <Route path="/analytics" element={<Analytics />} />
+          <Route path="/todo" element={<ToDoList />} />
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/profile" element={<ProfilePage isAdmin={isAdmin} />} />
